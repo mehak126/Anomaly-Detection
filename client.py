58c49d2e3a271fbe12245d2017539976a388af63
Mehak <mehak@Mehaks-MacBook-Pro.local>
2018-01-11 09:47:41 +0530

    COD310 commit

---
+++ client.py
@@ -0,0 +1,327 @@
+import socket
+import sys
+import math
+from copy import deepcopy
+import threading
+import random
+#import subprocess
+import time
+
+
+class Coordinate : #Class which stores x,y,z coordinate and the cluster of each point on the clustering graph
+	x = 0.0
+	y = 0.0
+	z = 0.0
+	c = 0
+
+
+	def __init__(self, i, j, k, cluster): #constructor
+		self.x = i
+		self.y = j
+		self.z = k
+		self.c = cluster
+
+#arrays for storing initial cluster points of server 1, 2 and 3 respectively 
+characteristic_points_s1 = []
+characteristic_points_s2 = []
+characteristic_points_s3 = []
+
+
+n = 500 #initial cluster points for each server
+t=5 #no. of threads
+k = 3 #no. of clusters
+
+data=[[Coordinate(0,0,0,0) for i in range(n)] for j in range(3)] #array to store coordinates of data points for each server
+means = [[Coordinate(0,0,0,0) for i in range(k)] for j in range(3)] #array to store coordinates of means of clusters for each server
+max_dist = [[0 for i in range(k)] for j in range(3)] #array to store data point at maximum distance from the cluster center for each server
+clusterCardinality = [[1 for i in range(n)] for j in range(3)] #array to store cardinality of each cluster for each server
+
+global lock
+global change 
+change = True
+terminate=0
+
+
+def main(server_id): #function which sets up a TCP connection with the server specified by the argument server_id and receives data from it
+	
+	#setting the server IP and Port for the TCP connection according to the id
+	if server_id == 0:
+		serverName = '10.184.13.145'
+		serverPort = 12345
+
+	elif server_id == 1:
+		serverName = '10.184.13.145'
+		serverPort = 12346
+
+	else:
+		serverName = '10.184.13.145'
+		serverPort = 12348
+
+
+	#Receiving text data from the server
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((serverName,serverPort))
+	ftp_sentence = 'Send ftp data'
+	s.send(ftp_sentence)	
+	ftp_data = s.recv(1024)
+	temp = s.recv(1024)	
+	while(temp != ''):
+		temp = s.recv(1024)
+		ftp_data += temp	
+	ftp_size = sys.getsizeof(ftp_data)
+	s.close()
+	
+
+	#Receiving image (jpg) data from the server
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((serverName,serverPort))
+	jpg_sentence = 'Send jpg data'
+	s.send(jpg_sentence)	
+	jpg_data = s.recv(1024)
+	temp = s.recv(1024)	
+	while(temp != ''):
+		temp = s.recv(1024)
+		jpg_data += temp
+	jpg_size = sys.getsizeof(jpg_data)
+	s.close()
+
+	#Receiving video data from the server
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((serverName,serverPort))	
+	vid_sentence = 'Send video data'
+	s.send(vid_sentence)	
+	vid_data = s.recv(1024)
+	temp = s.recv(1024)	
+	while(temp != ''):
+		temp = s.recv(1024)
+		vid_data += temp
+	vid_size = sys.getsizeof(vid_data)
+	s.close()
+
+	#calculating ratios of received data
+	ratio = [0.0] * 3 #0-> text, 1-> jpg, 2-> vid 
+	ratio[0] = ftp_size
+	ratio[1] = jpg_size
+	ratio[2] = vid_size
+
+	mini = min(min(ratio[0],ratio[1]),ratio[2])
+	for i in range(3):
+		ratio[i] = float(ratio[i]) / mini
+
+	#appending the ratio as a data point to the cluster points of the appropriate server
+	if server_id == 0:
+		characteristic_points_s1.append(Coordinate(ratio[0], ratio[1], ratio[2], k))
+	elif server_id == 1:
+		characteristic_points_s2.append(Coordinate(ratio[0], ratio[1], ratio[2], k))
+	else:
+		characteristic_points_s3.append(Coordinate(ratio[0], ratio[1], ratio[2], k))
+
+	return Coordinate(ratio[0],ratio[1],ratio[2],k)
+
+
+def calc_distance(point1, point2): #Function which calculates and returns the euclidean distance between two coordinates
+	return math.sqrt(math.pow((point1.x-point2.x),2) + math.pow((point1.y-point2.y),2) + math.pow((point1.z - point2.z),2))
+
+
+def initialize(server): #function for calculating initial cluster centers using kmeans++
+	totaldist = 0
+	newcentre = random.randint(0,n-1)
+	count=0
+	templist=deepcopy(data[server])
+	means[server][count]=templist[newcentre]
+	count+=1
+
+	while count < k :
+
+		totaldist=0
+		temp = []
+		dist = []
+		for i in range(n):
+			minimum = float('inf')
+			for j in range(count):
+				if calc_distance(data[server][i],means[server][j]) < minimum :
+					newcentre = j
+
+			#print("server = " + str(server) + " i = " + str(i) + " newcentre = " + str(newcentre))
+			dist.append(math.pow(calc_distance(data[server][i],means[server][newcentre]),2))
+			totaldist += dist[i]
+
+		for i in range(n):
+			if i == 0 :
+				temp.append(dist[i]/totaldist)
+			else :
+				temp.append(temp[i-1] + dist[i]/totaldist)
+
+		point = random.uniform(0,1)
+
+		for i in range(n):
+			if (point <= temp[i]):
+				#means.append(data[i])
+				fl = True
+				for j in range(count):
+					if(means[server][j].x == templist[i].x and means[server][j].y == templist[i].y and means[server][j].z == templist[i].z):
+						fl = False
+						break
+				if fl == False:
+					continue
+				means[server][count]=templist[i]
+				count += 1
+				break
+
+
+def updateMeans(server): #Calculating cluster means for the server specified by 'server'
+	templ=deepcopy(data[server])
+	for i in range(k):
+		means[server][i].x = 0
+		means[server][i].y = 0
+		means[server][i].z = 0
+		max_dist[server][i] = 0
+		clusterCardinality[server][i]=0
+
+	for i in range(len(templ)):
+		means[server][data[server][i].c].x += templ[i].x
+		means[server][data[server][i].c].y += templ[i].y
+		means[server][data[server][i].c].z += templ[i].z
+		clusterCardinality[server][data[server][i].c] += 1
+
+	for i in range(k):
+		if(clusterCardinality[server][i]!=0):
+			means[server][i].x /= clusterCardinality[server][i]
+			means[server][i].y /= clusterCardinality[server][i]
+			means[server][i].z /= clusterCardinality[server][i]
+
+	for i in range(len(templ)):
+		if(calc_distance(data[server][i],means[server][data[server][i].c]) > max_dist[server][data[server][i].c]):
+			max_dist[server][data[server][i].c] = calc_distance(data[server][i], means[server][data[server][i].c])	
+
+
+def cluster(ID,server): #Calculating cluster of the data points assigned to thread with ID "ID" for the server "server"
+	global change
+	global lock
+	start=ID * (n/t)
+	if ID==(t-1):
+		end=n
+	else:
+		end=start + (n/t)
+	for i in range(start,end):
+		min = float('inf')
+		for j in range(k):
+			temp = calc_distance(data[server][i],means[server][j])
+			if temp < min :
+				min = temp
+				newCluster = j
+		lock.acquire()
+		try:
+			if newCluster!=data[server][i].c:
+				change = True
+				data[server][i].c = newCluster
+		finally:
+			lock.release()
+	return
+
+def get_cluster(point,server): #Function to get cluster of the new data point and flag it as normal or anomalous
+
+	min_dist = float('inf')
+	anomalous = True
+
+	for i in range(len(means[server])):
+		if calc_distance(point, means[server][i]) <= (max_dist[server][i]*1.5) : #checking if the point lies within 5% of the farthest point from the cluster center
+			anomalous = False
+			if(calc_distance(point, means[server][i]) < min_dist):
+				point.c = i;
+				min_dist = calc_distance(point, means[server][i])
+
+
+	if anomalous == True :
+		print("\nAnomalous data detected in server " + str(server) + "\n")
+		#turning on the appropriate leds of the raspberry pi according to the server
+		# if server == 0:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		# elif server == 1:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# else:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		time.sleep(2)
+
+
+
+	else : #updating cluster center with the new point included
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		data[server].append(point)
+		updateMeans(server)
+			
+
+def printClusterMeans(server): #Function to print coordinates of means of clusters for the server specified by 'server'
+	print("Server " + str(server))
+	for i in range(len(means[server])):
+		print(str(means[server][i].x) + ", " + str(means[server][i].y) + ", " + str(means[server][i].z)  +" dist  "+ str(max_dist[server][i]) + " cardinality: " + str(clusterCardinality[server][i]))
+
+
+def kmeans(pointArray, server): #performing clustering of the points in the array 'pointArray' correspoinding to the server 'server'
+	global change
+	change = True
+	temp_list = deepcopy(pointArray)
+	for i in range(n):
+		data[server][i] = temp_list[i]
+
+	#clusterCardinality = [1] * len(characteristic_points_s1)
+	
+	temp_points=deepcopy(data[server])
+	for i in range(k):
+		means[server][i] = temp_points[i]
+
+	initialize(server)
+
+	global lock
+	lock=threading.Lock()
+	terminate = 0
+	while change:
+		change = False
+		terminate += 1
+		if terminate==10:
+			break
+		allThreads=[]
+		for i in range(t):
+			allThreads.append(threading.Thread(target=cluster, args=(i,server,))) 
+			allThreads[i].start()
+
+	   	for s in range(t):
+	   		allThreads[s].join()
+		updateMeans(server)	
+
+
+if __name__ == '__main__':
+
+		#turning off both leds of the raspberry pi
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+
+		#getting 'n' points for initial clustering from the 3 servers
+		for i in range(0,3):
+			for j in range(n):
+				point = main(i)
+
+		#Forming clusters of the initial points of each server
+		kmeans(characteristic_points_s1,0);
+		kmeans(characteristic_points_s2,1);
+		kmeans(characteristic_points_s3,2);
+
+		#printing cluster means of the 3 servers
+		for i in range(0,3):
+			printClusterMeans(i)
+
+		#Getting new data points from the servers and classifying them as normal or anomalous
+		while(1):
+		
+			for i in range(0,3):
+				point = main(i)
+				get_cluster(point,i)
+
+
+
+
+
+
+
---
+++ gen1.py
@@ -0,0 +1,85 @@
+import socket
+import select
+import sys
+import math
+import fractions
+import random
+
+serverPort = 12346
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+
+def main():
+	count = 0 #to keep count of number of times data has been sent so far
+	characteristic_ratios = [[0.33,0.33,0.33], [0.6,0.3,0.1], [0.2,0.1,0.4]] #0-> file, 1-> jpg, 2-> vid
+
+	serverSocket.listen(1)
+	print ('The server is ready')
+
+	while 1:
+		connectionSocket, addr = serverSocket.accept()
+
+		sentence = connectionSocket.recv(1024)
+
+		if sentence == 'Send ftp data':
+			print("Sending data to client " + str(addr))
+			count += 1
+
+			if (count>500) and (count%15 == 0): #sending anomalous data
+				ratio = [0.1,0.8,0.4]
+			else: #randomly choosing one of the three characteristic ratios
+				num = random.randint(0,2)
+				ratio = characteristic_ratios[num]		
+
+			data_vid0 = get_vid()
+			data_ftp0 = get_ftp()
+			data_jpg0 = get_jpg()
+
+			data_ftp = ""
+			data_jpg = ""
+			data_vid = ""	
+
+			#appending bytes of each of the files to get the file size in the appropriate ratio
+			for i in range(int(ratio[0]*1000)):
+				data_ftp += data_ftp0[i]
+
+			for i in range(int(ratio[1]*1000)):
+				data_jpg += data_jpg0[i]
+
+			for i in range(int(ratio[2]*1000)):
+				data_vid += data_vid0[i]
+
+			connectionSocket.send(data_ftp)
+			connectionSocket.close()			
+
+		if sentence == 'Send jpg data':
+			connectionSocket.send(data_jpg)
+			connectionSocket.close()
+
+		if sentence == 'Send video data':
+			connectionSocket.send(data_vid)
+			connectionSocket.close()		
+
+def get_ftp(): #reading and storing bytes of the text file
+	ftp_file = open('README','rb')
+	data_ftp = ftp_file.read()
+	ftp_file.close()
+
+	return data_ftp
+
+def get_jpg(): #reading and storing bytes of the image file
+	image = open("image3.png","rb")
+	data_jpg = image.read()
+
+	return data_jpg
+
+
+def get_vid(): #reading and storing bytes of the byte file
+	video = open("video2.mp4","rb")
+	data_vid = video.read()
+
+	return data_vid
+
+
+if __name__ == '__main__':
+		main()
