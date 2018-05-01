8c012edf5c8c6988bfb46da9f08c3c3cb1915a56
mehak126 <mehakpreet1212@gmail.com>
2018-05-01 11:53:00 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,8 +4,20 @@ import math
 from copy import deepcopy
 import threading
 import random
-import subprocess
+# import subprocess
 import time
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+class send_data:
+	data = "" 
+	flag = 0
+	def __init__(self,d,f):
+		self.data = d
+		self.flag = f
+
 
 
 class Coordinate : #Class which stores x,y,z coordinate and the cluster of each point on the clustering graph
@@ -26,8 +38,9 @@ characteristic_points_s1 = []
 characteristic_points_s2 = []
 characteristic_points_s3 = []
 
+counter = [0,0,0,0] #counter for TP,FP,TN,FN
 
-n = 500 #initial cluster points for each server
+n = 1000 #initial cluster points for each server
 t=5 #no. of threads
 k = 3 #no. of clusters
 
@@ -41,20 +54,23 @@ global change
 change = True
 terminate=0
 
+global anomalous_count
+anomalous_count = 0
+
 
 def main(server_id): #function which sets up a TCP connection with the server specified by the argument server_id and receives data from it
 	
 	#setting the server IP and Port for the TCP connection according to the id
 	if server_id == 0:
-		serverName = '10.184.13.145'
+		serverName = '127.0.0.1'
 		serverPort = 12345
 
 	elif server_id == 1:
-		serverName = '10.184.13.145'
+		serverName = '127.0.0.1'
 		serverPort = 12346
 
 	else:
-		serverName = '10.184.13.145'
+		serverName = '127.0.0.1'
 		serverPort = 12348
 
 
@@ -67,8 +83,11 @@ def main(server_id): #function which sets up a TCP connection with the server sp
 	temp = s.recv(1024)	
 	while(temp != ''):
 		temp = s.recv(1024)
-		ftp_data += temp	
-	ftp_size = sys.getsizeof(ftp_data)
+		ftp_data += temp
+	flag = ftp_data[0]	
+	if flag == 1:
+		anomalous_count += 1
+	ftp_size = sys.getsizeof(ftp_data[1:])
 	s.close()
 	
 
@@ -82,7 +101,7 @@ def main(server_id): #function which sets up a TCP connection with the server sp
 	while(temp != ''):
 		temp = s.recv(1024)
 		jpg_data += temp
-	jpg_size = sys.getsizeof(jpg_data)
+	jpg_size = sys.getsizeof(jpg_data[1:])
 	s.close()
 
 	#Receiving video data from the server
@@ -95,7 +114,7 @@ def main(server_id): #function which sets up a TCP connection with the server sp
 	while(temp != ''):
 		temp = s.recv(1024)
 		vid_data += temp
-	vid_size = sys.getsizeof(vid_data)
+	vid_size = sys.getsizeof(vid_data[1:])
 	s.close()
 
 	#calculating ratios of received data
@@ -116,7 +135,7 @@ def main(server_id): #function which sets up a TCP connection with the server sp
 	else:
 		characteristic_points_s3.append(Coordinate(ratio[0], ratio[1], ratio[2], k))
 
-	return Coordinate(ratio[0],ratio[1],ratio[2],k)
+	return (flag,Coordinate(ratio[0],ratio[1],ratio[2],k))
 
 
 def calc_distance(point1, point2): #Function which calculates and returns the euclidean distance between two coordinates
@@ -158,9 +177,7 @@ def initialize(server): #function for calculating initial cluster centers using 
 			else :
 				temp.append(temp[i-1] + dist[i]/totaldist)
 
-		point = random.uniform(0,1)	'''We have now chosen a random point between 0 and 1. We will see the position in temp in whose range we will find this point. 
-						This is as good as choosing a point at random, in a distribution poportional to distance squared.
-						'''
+		point = random.uniform(0,1)
 		for i in range(n):
 			if (point <= temp[i]):
 				#means.append(data[i])
@@ -226,13 +243,14 @@ def cluster(ID,server): #Calculating cluster of the data points assigned to thre
 			lock.release()
 	return
 
-def get_cluster(point,server): #Function to get cluster of the new data point and flag it as normal or anomalous
+def get_cluster(point,server,flag): #Function to get cluster of the new data point and flag it as normal or anomalous
 
+	ret_type = 0
 	min_dist = float('inf')
 	anomalous = True
 
 	for i in range(len(means[server])):
-		if calc_distance(point, means[server][i]) <= (max_dist[server][i]*1.1) : #checking if the point lies within 10% of the farthest point from the cluster center
+		if calc_distance(point, means[server][i]) <= (max_dist[server][i]*1.05) : #checking if the point lies within 10% of the farthest point from the cluster center
 			anomalous = False
 			if(calc_distance(point, means[server][i]) < min_dist):
 				point.c = i;
@@ -241,25 +259,39 @@ def get_cluster(point,server): #Function to get cluster of the new data point an
 
 	if anomalous == True :
 		print("\nAnomalous data detected in server " + str(server) + "\n")
-		#turning on the appropriate leds of the raspberry pi according to the server
-		if server == 0:
-			subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
-		elif server == 1:
-			subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		if flag == "0":
+			#print("FALSE ANOMALY!!!!")
+			counter[3]+=1
+			ret_type = 3
 		else:
-			subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
-			subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
-		time.sleep(2)
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+			counter[2]+=1
+			ret_type = 2
+
+		#turning on the appropriate leds of the raspberry pi according to the server
+		# if server == 0:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		# elif server == 1:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# else:
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# 	subprocess.call('echo 1 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		# time.sleep(2)
 
 
 
 	else : #updating cluster center with the new point included
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		if flag == "1":
+		#	print("FALSE NORMAL!!!")
+			counter[1]+=1
+			ret_type = 1
+		else:
+			counter[0]+=1
+			ret_type = 0
 		data[server].append(point)
 		updateMeans(server)
+	return ret_type
 			
 
 def printClusterMeans(server): #Function to print coordinates of means of clusters for the server specified by 'server'
@@ -296,16 +328,16 @@ def kmeans(pointArray, server): #performing clustering of the points in the arra
 			allThreads.append(threading.Thread(target=cluster, args=(i,server,))) 
 			allThreads[i].start()
 
-	   	for s in range(t):
-	   		allThreads[s].join()
+		for s in range(t):
+			allThreads[s].join()
 		updateMeans(server)	
 
 
 if __name__ == '__main__':
 
 		#turning off both leds of the raspberry pi
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
-		subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led0/brightness', shell = True)
+		# subprocess.call('echo 0 | sudo tee /sys/class/leds/led1/brightness', shell = True)
 
 		#getting 'n' points for initial clustering from the 3 servers
 		for i in range(0,3):
@@ -317,16 +349,243 @@ if __name__ == '__main__':
 		kmeans(characteristic_points_s2,1);
 		kmeans(characteristic_points_s3,2);
 
+		plt.ion()
+		
+
+		fig = plt.figure(1)
+		ax1 = fig.add_subplot(111, projection='3d')
+		xs = [val.x for val in characteristic_points_s1]
+		xs = list(map(float,xs))
+		xs = np.array(xs)
+
+		
+		ys = [val.y for val in characteristic_points_s1]
+		ys = list(map(float,ys))
+		ys = np.array(ys)
+
+		zs = [val.z for val in characteristic_points_s1]
+		zs = list(map(float,zs))
+		zs = np.array(zs)
+
+		for i in range(len(xs)):
+			#print(characteristic_points_s1[i].c)
+			if data[0][i].c == 0:
+				ax1.scatter(data[0][i].x, data[0][i].y, data[0][i].z, c='y', marker = 'o',zorder = 10)
+			elif data[0][i].c == 1:
+				ax1.scatter(data[0][i].x, data[0][i].y, data[0][i].z, c='grey', marker = 'o',zorder = 10)
+			else:
+				ax1.scatter(data[0][i].x, data[0][i].y, data[0][i].z, c='pink', marker = 'o',zorder = 10)
+
+
+		plt.draw()
+		#plt.hold(True)
+		plt.pause(2)
+		# plt.clf()
+	#	plt.show()
+		# 	plt.draw()
+		# 	plt.pause(0.2)
+		# plt.clf
+
+
+		# X = np.linspace( ,  2 * np.max(theta_0) - np.min(theta_0)  , num = 200)
+		# Y = np.linspace( np.min(theta_1) -1 ,  np.max(theta_1) +1  , num = 200)
+
+
+
+		# fig = plt.figure()
+		# ax = fig.add_subplot(111, projection='3d')
+		# xs = [val.x for val in characteristic_points_s2]
+		# xs = list(map(float,xs))
+		# xs = np.array(xs)
+
+		
+		# ys = [val.y for val in characteristic_points_s2]
+		# ys = list(map(float,ys))
+		# ys = np.array(ys)
+
+		# zs = [val.z for val in characteristic_points_s2]
+		# zs = list(map(float,zs))
+		# zs = np.array(zs)
+
+		# #for i in range(len(xs)):
+		# ax.scatter(xs, ys, zs, c='r', marker = 'o')
+		# plt.show()
+		# plt.pause(0.2)
+		# plt.clf()
+		# plt.close()
+
+
+		# fig = plt.figure()
+		# ax = fig.add_subplot(111, projection='3d')
+		# xs = [val.x for val in characteristic_points_s3]
+		# xs = list(map(float,xs))
+		# xs = np.array(xs)
+
+		
+		# ys = [val.y for val in characteristic_points_s3]
+		# ys = list(map(float,ys))
+		# ys = np.array(ys)
+
+		# zs = [val.z for val in characteristic_points_s3]
+		# zs = list(map(float,zs))
+		# zs = np.array(zs)
+
+		# #for i in range(len(xs)):
+		# ax.scatter(xs, ys, zs, c='g', marker = 'o')
+		# plt.show()
+		# plt.close()
+
+
+
+		fig = plt.figure(2)
+		ax2 = fig.add_subplot(111, projection='3d')
+		xs = [val.x for val in characteristic_points_s2]
+		xs = list(map(float,xs))
+		xs = np.array(xs)
+
+		
+		ys = [val.y for val in characteristic_points_s2]
+		ys = list(map(float,ys))
+		ys = np.array(ys)
+
+		zs = [val.z for val in characteristic_points_s2]
+		zs = list(map(float,zs))
+		zs = np.array(zs)
+
+		for i in range(len(xs)):
+			#print(characteristic_points_s1[i].c)
+			if data[1][i].c == 0:
+				ax2.scatter(data[1][i].x, data[1][i].y, data[1][i].z, c='y', marker = 'o',zorder = 10)
+			elif data[1][i].c == 1:
+				ax2.scatter(data[1][i].x, data[1][i].y, data[1][i].z, c='grey', marker = 'o',zorder = 10)
+			else:
+				ax2.scatter(data[1][i].x, data[1][i].y, data[1][i].z, c='pink', marker = 'o',zorder = 10)
+
+		plt.draw()
+		#plt.hold(True)
+		plt.pause(2)
+		# plt
+	#	plt.clf()
+		
+
+	#	plt.show()
+
+
+
+
+
+
+
+
+		fig = plt.figure(3)
+		ax3 = fig.add_subplot(111, projection='3d')
+		xs = [val.x for val in characteristic_points_s3]
+		xs = list(map(float,xs))
+		xs = np.array(xs)
+
+		
+		ys = [val.y for val in characteristic_points_s3]
+		ys = list(map(float,ys))
+		ys = np.array(ys)
+
+		zs = [val.z for val in characteristic_points_s3]
+		zs = list(map(float,zs))
+		zs = np.array(zs)
+
+		for i in range(len(xs)):
+			#print(characteristic_points_s1[i].c)
+			if data[2][i].c == 0:
+				ax3.scatter(data[2][i].x, data[2][i].y, data[2][i].z, c='y', marker = 'o',zorder = 10)
+			elif data[2][i].c == 1:
+				ax3.scatter(data[2][i].x, data[2][i].y, data[2][i].z, c='grey', marker = 'o',zorder = 10)
+			else:
+				ax3.scatter(data[2][i].x, data[2][i].y, data[2][i].z, c='pink', marker = 'o',zorder = 10)
+		
+		plt.draw()
+		#plt.hold(True)
+		plt.pause(2)
+		# plt.clf()
+	#	plt.close()
+
+		#labels
+		x = [data[0][0].x]
+		y = [data[0][0].y]
+		z = [data[0][0].z]
+		ax1.plot(x,y,z,'g',marker = 'o',label = "normal")
+		ax1.plot(x,y,z,'b',marker = 'o',label = "false normal")
+		ax1.plot(x,y,z,'r',marker = 'o',label = "anomaly")
+		ax1.plot(x,y,z,'orange',marker = 'o',label = "false anomaly")
+		ax1.plot(x,y,z,'pink',marker = 'o')
+		ax1.legend()
+
+
+
+		x = [data[1][0].x]
+		y = [data[1][0].y]
+		z = [data[1][0].z]
+		ax2.plot(x,y,z,'g',marker = 'o',label = "normal")
+		ax2.plot(x,y,z,'b',marker = 'o',label = "false normal")
+		ax2.plot(x,y,z,'r',marker = 'o',label = "anomaly")
+		ax2.plot(x,y,z,'orange',marker = 'o',label = "false anomaly")
+		ax2.plot(x,y,z,'pink',marker = 'o')
+		ax2.legend()
+
+		
+		x = [data[2][0].x]
+		y = [data[2][0].y]
+		z = [data[2][0].z]
+		ax3.plot(x,y,z,'g',marker = 'o',label = "normal")
+		ax3.plot(x,y,z,'b',marker = 'o',label = "false normal")
+		ax3.plot(x,y,z,'r',marker = 'o',label = "anomaly")
+		ax3.plot(x,y,z,'orange',marker = 'o',label = "false anomaly")
+		ax3.plot(x,y,z,'pink',marker = 'o')
+		ax3.legend()
+
+
+
+
+
+
 		#printing cluster means of the 3 servers
 		for i in range(0,3):
 			printClusterMeans(i)
 
+		numpoints = 0
 		#Getting new data points from the servers and classifying them as normal or anomalous
-		while(1):
-		
+		while(numpoints<1000):	
+			numpoints += 1
 			for i in range(0,3):
-				point = main(i)
-				get_cluster(point,i)
+				(flag,point) = main(i)
+				ret_type = get_cluster(point,i,flag)  #0-> normal, #1-> false normal, #2 -> anomalous, #3 -> false anomaly
+				plt.figure(i+1)
+			#	ax = fig.add_subplot(111,projection = '3d')
+			#	ax = fig.add_subplot()
+				#ax = plt.subplot(111)
+				#print(ret_type)
+				c = 'b'
+				if ret_type == 0: #normal
+					c = 'g'
+				elif ret_type == 1: #false normal
+					c = 'b'
+				elif ret_type == 2: #anomaly
+					c = 'r'
+				else:
+					c = 'orange' #false anomaly
+
+
+
+				if i == 0:
+					ax1.scatter(point.x, point.y, point.z, c=c, marker = 'o',zorder = 0)
+				elif i == 1:
+					ax2.scatter(point.x, point.y, point.z, c=c, marker = 'o',zorder = 0)
+				else:
+					ax3.scatter(point.x, point.y, point.z, c=c, marker = 'o',zorder = 0)
+				plt.draw()
+				plt.pause(0.0000000001)
+
+		output = open("stats.csv","w")
+		output.write("True normal,False normal,True anomaly,False anomaly, actual anomalous\n")
+		output.write(str(counter[0]) + "," + str(counter[1]) + "," + str(counter[2]) + "," + str(counter[3]) + "," +str(anomalous_count))
 
 
 
